8fe3000dced77151884187cea748bf5f27a11a0e
aberkow <berkowitz.clarinet@gmail.com>
2016-02-24 11:26:48 -0500

    basic layout no css starting js

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var quiz = {
+    question: [
+        var question1 = {
+            
+        }
+    ];
+}
+
+
+//var quiz = {
+//    1 - array of objects with questions, answers, and correct/incorrect info
+//     2 - counter to keep track of how many questions have been answered correctly.
+//      
+//};
+
+$(document).ready(function() {
+    /*just show the intro screen*/
+    $('.quiz').hide();
+    $('.final').hide();
+    
+    /*event handler area*/
+    
+    //start the game
+    $('.intro__start').click(function() {
+        $('.quiz').show();
+        $('.intro').hide();    
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    /*function area*/
+});
