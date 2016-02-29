87c3f43ef7927b7bddd65f8418da42dbe70ba741
aberkow <berkowitz.clarinet@gmail.com>
2016-02-29 15:39:01 -0500

    trying to make a quizState object hold relevant data

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 var quizIndexCounter = 0;
 
+var currentQuizState = {};
+
 var quiz = [
   new Question("What is Fragile X Syndrome?", ["A genetic disorder.", "A behavioral disorder.", "A physical disability.", "A mental disorder"], 0, "Fragile X Syndrome is a genetic disorder that..."),
   
@@ -18,32 +20,8 @@ function Question(question, answerArr, correctAnswer, infoText) {
     this.question = question;
     this.answerArr = answerArr;
     this.correctAnswer = answerArr[correctAnswer];
-    this.userAnswer = null;
     this.infoText = infoText;
 }
-
-/* questions */
-//quiz.push(new Question("What is Fragile X Syndrome?", ["A genetic disorder.", "A behavioral disorder.", "A physical disability.", "A mental disorder"], 0, "Fragile X Syndrome is a genetic disorder that..."));
-//
-//quiz.push(new Question("What are the approximate odds of being a carrier for Fragile X Syndrome?", ["1 in 1000 for males and females", "1 in 600 for males and 1 in 1000 for females", "1 in 500 for males and 1 in 150 for females", "1 in 150 for males and 1 in 500 for females"], 2, "The odds of being a carrier are higer for females because they have two X chromosomes."));
-//
-//quiz.push(new Question("What are the approximate odds of having full mutation Fragile X Syndrome?", ["1 in 300 for males and 1 in 500 for females", "1 in 2000 for males and 1 in 1000 for females", "1 in 3000 for males and 1 in 1500 for females", "1 in 4000 for males and 1 in 6000 for females"], 3, "Fragile X syndrome is more common in males and can effect them more strongly."));
-//
-//quiz.push(new Question("Fragile X Syndrome is the most common known cause of __________", ["Autism Spectrum Disorder", "Attention Deficit Disorder", "Inherited Intellectual Disability", "Dislexia"], 2, "A person with an intellectual disability has more difficulty learning and functioning in day to day life compared with a typical person."));
-//
-//quiz.push(new Question("How are people commonly diagnosed with FXS?", ["Physical exam", "Blood test", "Psychological evaluation", "Family history"], 1, "Both carriers and people with FXS are diagnosed through a genetic test usually from a blood sample."));
-
-/*array representing the entire quiz*/
-//var quiz = [
-////    questionOne,
-////    questionTwo,
-////    questionThree,
-////    questionFour,
-////    questionFive
-//]
-
-//var answerChoiceItem = 0;
-//var answerChoices = []
 
 $(document).ready(function() {
     /*just show the intro screen*/
@@ -54,12 +32,14 @@ $(document).ready(function() {
  
     /*start the game and get the first question/answer set ready*/
     $('.intro__start').click(function() {
-        $('.quiz').show();
-        $('.intro').hide();
+      startQuiz();  
+      $('.quiz').show();
+      $('.intro').hide();
       
       displayQuestion(quiz[quizIndexCounter]);
       //createAnswerChoices(quiz[quizIndexCounter]);
-      questionCounter();  
+      questionCounter(); 
+      
     });
   /*cycle back to the first question*/  
   $('.restart').click(function() {
@@ -75,7 +55,15 @@ $(document).ready(function() {
   });
 
     /*function area*/  
-   function questionCounter() {
+  function startQuiz() {
+    currentQuizState.question = null;
+    currentQuizState.userChoice = null;
+    currentQuizState.answers = null;
+    currentQuizState.correctChoice = null;
+    currentQuizState.numberCorrect = 0
+  } 
+  
+  function questionCounter() {
     //either increment the counter or hide the quiz and show last screen/reset the counter.
     if (quizIndexCounter < quiz.length - 1) { 
       quizIndexCounter++;
